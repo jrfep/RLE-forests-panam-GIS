5dea64f68210560ad694b945a5b96f776ab4af17
jrfep <jr.ferrer.paris@gmail.com>
2023-04-05 21:23:52 +1000

    copy assessment data from macrogroups table

--- workflow/export-macrogroup-map-to-vector.sh
+++ workflow/export-macrogroup-map-to-vector.sh
@@ -11,16 +11,11 @@ Rscript --vanilla -e 'load("MacrogroupsCountry.rda"); write.csv(Macrogroups.Glob
 
 module add gdal
 
-ogr2ogr -f CSV query-IVC-sam \
-    $GISDATA/vegetation/regional/IVC-EcoVeg/SAM/commondata/raster_data/SouthAmerica_IVC_MacroGroups_potential_NatureServe_v7_270m.tif.vat.dbf \
-   -sql "SELECT Value,macrogroup,mg_hierarc,mg_key FROM \"SouthAmerica_IVC_MacroGroups_potential_NatureServe_v7_270m.tif.vat\""
-
-ogr2ogr -f CSV query-IVC-nac \
-    $GISDATA/vegetation/regional/IVC-EcoVeg/NAC/commondata/raster_data/NorthAmerica_Caribbean_IVC_MacroGroups_potential_NatureServe_v5_270m.tif.vat.dbf \
-    -sql "SELECT Value,macrogroup,mg_hierarc,mg_key FROM \"NorthAmerica_Caribbean_IVC_MacroGroups_potential_NatureServe_v5_270m.tif.vat\""
-
-grep M${MCDG} query-IVC-sam/SouthAmerica_IVC_MacroGroups_potential_NatureServe_v7_270m.tif.vat.csv | \
-    awk -F "," '{ print $2}' 
+# this only has information on the clasification
+#ogr2ogr -f CSV query-IVC-nac \
+#    $GISDATA/vegetation/regional/IVC-EcoVeg/NAC/commondata/raster_data/NorthAmerica_Caribbean_IVC_MacroGroups_potential_NatureServe_v5_270m.tif.vat.dbf \
+#    -sql "SELECT Value,macrogroup,mg_hierarc,mg_key FROM \"NorthAmerica_Caribbean_IVC_MacroGroups_potential_NatureServe_v5_270m.tif.vat\""
+#grep M${MCDG} query-IVC-sam/SouthAmerica_IVC_MacroGroups_potential_NatureServe_v7_270m.tif.vat.csv | awk -F "," '{ print $2}' 
 
 
 mkdir -p vector-data-potential-dist
@@ -30,11 +25,7 @@ cd vector-data-potential-dist
 
 for MCDG in 640 # 563 572
 do
-
-    MGFULL=$(awk -F "," '/'$MCDG'/{ print $3 }' ../query-IVC-sam/SouthAmerica_IVC_MacroGroups_potential_NatureServe_v7_270m.tif.vat.csv)
-    MGNAME=$(awk -F "," '/'$MCDG'/{ print $2 }' ../query-IVC-sam/SouthAmerica_IVC_MacroGroups_potential_NatureServe_v7_270m.tif.vat.csv)
-    IUCNCAT=$(awk  -vFPAT='([^,]*)|("[^"]+")' -vOFS=, '/'$MCDG'/{ print $22" as IUCN_CAT, "$23" as IUCN_BOUNDS, "$24" as IUCN_criteria" }' ../macrogroups_global.csv| sed -e s/NA/NULL/)
-
+    IUCNCAT=$(awk  -vFPAT='([^,]*)|("[^"]+")' -vOFS=, '/'$MCDG'/{ print $5" AS mg_hierarc, "$22" as IUCN_CAT, "$23" as IUCN_BOUNDS, "$24" as IUCN_criteria" }' ../macrogroups_global.csv| sed -e s/NA/NULL/)
 
     gdal_calc.py -A $WORKDIR/IVC_NS_v7_270m_robin.tif \
     --outfile=M${MCDG}.tif \
@@ -47,7 +38,7 @@ do
             M${MCDG}.tif M${MCDG}-${res}km.tif
         gdal_polygonize.py M${MCDG}-${res}km.tif M${MCDG}-${res}km.gpkg -f "GPKG"
         ogr2ogr M${MCDG}-${res}km-union.gpkg  M${MCDG}-${res}km.gpkg \
-        -dialect sqlite -sql "SELECT 'M${MCDG}' AS mg_key, '${MGFULL}' AS mg_hierarc, $IUCNCAT, ST_union(geom) AS geom FROM out " \
+        -dialect sqlite -sql "SELECT 'M${MCDG}' AS mg_key, $IUCNCAT, ST_union(geom) AS geom FROM out " \
         -nln "rle_assessment" -t_srs "EPSG:4326" -mo CREATOR="JR Ferrer-Paris" \
         -mo CITATION="Ferrer-Paris, J. R., Zager, I., Keith, D. A., Oliveira-Miranda, M., Rodríguez, J. P., Josse, C., González-Gil, M., Miller, R. M., Zambrana-Torrelio, C., & Barrow, E. An ecosystem risk assessment of temperate and tropical forests of the Americas with an outlook on future conservation strategies. Conserv. Lett. 12.. https://doi.org/10.1111/conl.12623"
     done
